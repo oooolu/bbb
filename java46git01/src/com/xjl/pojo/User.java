cd75d0a5a5e724feb26a1ad461c930d1c47aaea2
xjl <1952435391@qq.com>
2021-01-19 17:41:52 +0800

    sadwsdawd

--- java46git01/src/com/xjl/pojo/User.java
+++ java46git01/src/com/xjl/pojo/User.java
@@ -6,4 +6,31 @@ public class User implements Serializable {
     private int id;
     private String name;
     private String pwd;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
